03493c719c80798d50b1e5a783c67db258629c3c
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-09-18 11:47:17 +0200

    Add debian control files

--- setup.py
+++ setup.py
@@ -21,16 +21,16 @@ along with PyHunspell.  If not, see <http://www.gnu.org/licenses/>.
 from distutils.core import setup, Extension
 
 main = Extension('hunspell', 
-			define_macros = [('_LINUX',None)],
-			libraries = ['hunspell'],
-			include_dirs = ['/usr/include/hunspell'],
-			sources = ['hunspell.c'],
-			extra_compile_args = ['-Wall'])
+	define_macros = [('_LINUX',None)],
+	libraries = ['hunspell'],
+	include_dirs = ['/usr/include/hunspell'],
+	sources = ['hunspell.c'],
+	extra_compile_args = ['-Wall'])
 
 setup(name = "hunspell",
 	version = "0.1",
 	description = "Module for the Hunspell spellchecker engine",
-    author="Sayamindu Dasgupta",
-    author_email="sayamindu@gmail.com",
-    url="http://code.google.com/p/pyhunspell",
+	author="Sayamindu Dasgupta",
+	author_email="sayamindu@gmail.com",
+	url="http://code.google.com/p/pyhunspell",
 	ext_modules = [main])
