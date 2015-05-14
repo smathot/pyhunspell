2177cc17bbc80d046f22c480a4b17679943931a0
Nat Picker <tallforasmurf@yahoo.com>
2015-05-14 13:33:52 -0700

    changes to compile with MSVC2010 and Windows 64-bit

--- setup.py
+++ setup.py
@@ -11,21 +11,23 @@ the Free Software Foundation, either version 3 of the License, or
 
 PyHunspell is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with PyHunspell. If not, see <http://www.gnu.org/licenses/>.
+along with PyHunspell.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from distutils.core import setup, Extension
 
 main = Extension('hunspell',
-                 define_macros=[('_LINUX', None)],
-                 libraries=['hunspell'],
-                 include_dirs=['/usr/include/hunspell'],
+                 define_macros=[('HUNSPELL_STATIC', None)],
+#                 library_dirs=['V:/hunspell-1.3.3/src/win_api/Release/libhunspell'],
+                 library_dirs=['V:/hunspell-1.3.3/src/win_api/x64/Release/libhunspell'],
+                 libraries=['libhunspell'],
+                 include_dirs=['V:/hunspell-1.3.3/src/hunspell'],
                  sources=['hunspell.c'],
-                 extra_compile_args=['-Wall'])
+                 extra_compile_args=['/MT'])
 
 setup(name="hunspell",
       version="0.3.2",
