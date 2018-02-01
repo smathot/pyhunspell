c5cfab598530701557d2f10f00322089bc998947
Benoît Latinier <benoit@latinier.fr>
2018-02-01 23:27:05 +0100

    Fix sor warning and make the package wheel compatible

--- setup.py
+++ setup.py
@@ -18,27 +18,27 @@ You should have received a copy of the GNU General Public License
 along with PyHunspell. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import platform
 
 main_module_kwargs = {"sources": ['hunspell.cpp'],
                       "language": "c++"}
 if platform.system() == "Windows":
-    main_module_kwargs['macros'] = [('HUNSPELL_STATIC', None)]
+    main_module_kwargs['define_macros'] = [('HUNSPELL_STATIC', None)]
     main_module_kwargs['libraries'] = ['libhunspell']
     main_module_kwargs['include_dirs'] = ['V:/hunspell-1.3.3/src/hunspell']
     main_module_kwargs['library_dirs'] = ['V:/hunspell-1.3.3/src/win_api/x64/Release/libhunspell']
-    main_module_kwargs['compile_args'] = ['/MT']
+    main_module_kwargs['extra_compile_args'] = ['/MT']
 elif platform.system() == "Darwin":
-    main_module_kwargs['macros'] = [('_LINUX', None)]
+    main_module_kwargs['define_macros'] = [('_LINUX', None)]
     main_module_kwargs['libraries'] = ['hunspell']
     main_module_kwargs['include_dirs'] = '/usr/local/Cellar/hunspell/1.6.2/include/hunspell',
-    main_module_kwargs['compile_args'] = ['-Wall']
+    main_module_kwargs['extra_compile_args'] = ['-Wall']
 else:
-    main_module_kwargs['macros'] = [('_LINUX', None)]
+    main_module_kwargs['define_macros'] = [('_LINUX', None)]
     main_module_kwargs['libraries'] = ['hunspell']
     main_module_kwargs['include_dirs'] = '/usr/include/hunspell',
-    main_module_kwargs['compile_args'] = ['-Wall']
+    main_module_kwargs['extra_compile_args'] = ['-Wall']
 
 main = Extension('hunspell', **main_module_kwargs)
 
